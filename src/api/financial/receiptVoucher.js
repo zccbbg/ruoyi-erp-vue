b9e52bc3820e334c9e5cb9442953a591599df793
zcc <zccbbg@qq.com>
2025-02-14 09:37:25 +0800

    feat(financial): 添加收款单功能
    - 创建收款单列表页面，包括搜索、新增、修改、删除和导出功能
    - 实现收款单相关的 API 接口，包括查询列表、查询详情、新增、修改和删除
    - 优化日期选择器和分页组件的使用
    - 添加表单验证规则

---
+++ src/api/financial/receiptVoucher.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询收款单列表
+export function listReceiptVoucher(query) {
+  return request({
+    url: '/financial/receiptVoucher/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询收款单详细
+export function getReceiptVoucher(id) {
+  return request({
+    url: '/financial/receiptVoucher/' + id,
+    method: 'get'
+  })
+}
+
+// 新增收款单
+export function addReceiptVoucher(data) {
+  return request({
+    url: '/financial/receiptVoucher',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改收款单
+export function updateReceiptVoucher(data) {
+  return request({
+    url: '/financial/receiptVoucher',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除收款单
+export function delReceiptVoucher(id) {
+  return request({
+    url: '/financial/receiptVoucher/' + id,
+    method: 'delete'
+  })
+}
---
+++ src/views/financial/receiptVoucher/index.vue
@@ -0,0 +1,333 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <el-form :model="queryParams" ref="queryRef" :inline="true" label-width="68px">
+              <el-form-item label="编号" prop="voucherNo">
+                <el-input
+                  v-model="queryParams.voucherNo"
+                  placeholder="请输入编号"
+                  clearable
+                  @keyup.enter="handleQuery"
+                />
+              </el-form-item>
+              <el-form-item label="收款日期" style="width: 308px">
+                <el-date-picker
+                  v-model="daterangeTransDate"
+                  value-format="YYYY-MM-DD HH:mm:ss"
+                  type="daterange"
+                  range-separator="-"
+                  start-placeholder="开始日期"
+                  :default-time="[new Date(2000, 1, 1, 0, 0, 0), new Date(2000, 1, 1, 23, 59, 59)]"
+                ></el-date-picker>
+              </el-form-item>
+              <el-form-item label="往来单位id" prop="merchantId">
+                <el-input
+                  v-model="queryParams.merchantId"
+                  placeholder="请输入往来单位id"
+                  clearable
+                  @keyup.enter="handleQuery"
+                />
+              </el-form-item>
+              <el-form-item label="银行账户id" prop="bankAccountId">
+                <el-input
+                  v-model="queryParams.bankAccountId"
+                  placeholder="请输入银行账户id"
+                  clearable
+                  @keyup.enter="handleQuery"
+                />
+              </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+            <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card class="mt20">
+
+      <el-row :gutter="10" class="mb8" type="flex" justify="space-between">
+        <el-col :span="6"><span style="font-size: large">收款单</span></el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['financial:receiptVoucher:add']"
+          >新增</el-button>
+          <el-button
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['financial:receiptVoucher:export']"
+          >导出</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table v-loading="loading" :data="receiptVoucherList" border class="mt20">
+            <el-table-column label="" prop="id" v-if="true"/>
+            <el-table-column label="编号" prop="voucherNo" />
+            <el-table-column label="收款日期" align="center" prop="transDate" width="180">
+              <template #default="scope">
+                <span>{{ parseTime(scope.row.transDate, '{y}-{m}-{d}') }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="往来单位id" prop="merchantId" />
+            <el-table-column label="银行账户id" prop="bankAccountId" />
+            <el-table-column label="支付金额" prop="paidAmount" />
+            <el-table-column label="优惠金额" prop="discountAmount" />
+            <el-table-column label="余额变动" prop="balanceChange" />
+            <el-table-column label="交易前余额" prop="beforeBalance" />
+            <el-table-column label="交易后余额" prop="afterBalance" />
+            <el-table-column label="审核状态" prop="checkedStatus" />
+            <el-table-column label="备注" prop="remark" />
+        <el-table-column label="操作" align="right" class-name="small-padding fixed-width">
+          <template #default="scope">
+            <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['financial:receiptVoucher:edit']">修改</el-button>
+            <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['financial:receiptVoucher:remove']">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-row>
+        <pagination
+          v-show="total>0"
+          :total="total"
+          v-model:page="queryParams.pageNum"
+          v-model:limit="queryParams.pageSize"
+          @pagination="getList"
+        />
+      </el-row>
+
+    </el-card>
+    <!-- 添加或修改收款单对话框 -->
+    <el-drawer :title="title" v-model="open" size="50%" append-to-body>
+      <el-form ref="receiptVoucherRef" :model="form" :rules="rules" label-width="80px">
+              <el-form-item label="编号" prop="voucherNo">
+                <el-input v-model="form.voucherNo" placeholder="请输入编号" />
+              </el-form-item>
+              <el-form-item label="收款日期" prop="transDate">
+                <el-date-picker clearable
+                                v-model="form.transDate"
+                                type="datetime"
+                                value-format="YYYY-MM-DD HH:mm:ss"
+                                placeholder="请选择收款日期">
+                </el-date-picker>
+              </el-form-item>
+              <el-form-item label="往来单位id" prop="merchantId">
+                <el-input v-model="form.merchantId" placeholder="请输入往来单位id" />
+              </el-form-item>
+              <el-form-item label="银行账户id" prop="bankAccountId">
+                <el-input v-model="form.bankAccountId" placeholder="请输入银行账户id" />
+              </el-form-item>
+              <el-form-item label="支付金额" prop="paidAmount">
+                <el-input v-model="form.paidAmount" placeholder="请输入支付金额" />
+              </el-form-item>
+              <el-form-item label="优惠金额" prop="discountAmount">
+                <el-input v-model="form.discountAmount" placeholder="请输入优惠金额" />
+              </el-form-item>
+              <el-form-item label="余额变动" prop="balanceChange">
+                <el-input v-model="form.balanceChange" placeholder="请输入余额变动" />
+              </el-form-item>
+              <el-form-item label="交易前余额" prop="beforeBalance">
+                <el-input v-model="form.beforeBalance" placeholder="请输入交易前余额" />
+              </el-form-item>
+              <el-form-item label="交易后余额" prop="afterBalance">
+                <el-input v-model="form.afterBalance" placeholder="请输入交易后余额" />
+              </el-form-item>
+              <el-form-item label="审核人" prop="checkedBy">
+                <el-input v-model="form.checkedBy" placeholder="请输入审核人" />
+              </el-form-item>
+              <el-form-item label="备注" prop="remark">
+                <el-input v-model="form.remark" placeholder="请输入备注" />
+              </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-drawer>
+  </div>
+</template>
+
+<script setup name="ReceiptVoucher">
+  import { listReceiptVoucher, getReceiptVoucher, delReceiptVoucher, addReceiptVoucher, updateReceiptVoucher } from "@/api/financial/receiptVoucher";
+
+const { proxy } = getCurrentInstance();
+
+  const receiptVoucherList = ref([]);
+  const open = ref(false);
+  const buttonLoading = ref(false);
+  const loading = ref(true);
+  const ids = ref([]);
+  const total = ref(0);
+  const title = ref("");
+    const daterangeTransDate = ref([]);
+
+  const data = reactive({
+    form: {},
+    queryParams: {
+      pageNum: 1,
+      pageSize: 10,
+    voucherNo: undefined,
+    transDate: undefined,
+    merchantId: undefined,
+    bankAccountId: undefined,
+  },
+  rules: {
+    id: [
+      { required: true, message: "不能为空", trigger: "blur" }
+    ],
+    voucherNo: [
+      { required: true, message: "编号不能为空", trigger: "blur" }
+    ],
+    transDate: [
+      { required: true, message: "收款日期不能为空", trigger: "blur" }
+    ],
+    merchantId: [
+      { required: true, message: "往来单位id不能为空", trigger: "blur" }
+    ],
+    bankAccountId: [
+      { required: true, message: "银行账户id不能为空", trigger: "blur" }
+    ],
+    paidAmount: [
+      { required: true, message: "支付金额不能为空", trigger: "blur" }
+    ],
+    remark: [
+      { required: true, message: "备注不能为空", trigger: "blur" }
+    ]
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询收款单列表 */
+function getList() {
+  loading.value = true;
+    queryParams.value.params = {};
+  if (null != daterangeTransDate && '' != daterangeTransDate) {
+    queryParams.value.params["beginTransDate"] = daterangeTransDate.value[0];
+    queryParams.value.params["endTransDate"] = daterangeTransDate.value[1];
+  }
+  listReceiptVoucher(queryParams.value).then(response => {
+  receiptVoucherList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    voucherNo: null,
+    transDate: null,
+    merchantId: null,
+    bankAccountId: null,
+    paidAmount: null,
+    discountAmount: null,
+    balanceChange: null,
+    beforeBalance: null,
+    afterBalance: null,
+    checkedStatus: null,
+    checkedBy: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null,
+    remark: null
+  };
+  proxy.resetForm("receiptVoucherRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+    /** 重置按钮操作 */
+    function resetQuery() {
+      daterangeTransDate.value = [];
+    proxy.resetForm("queryRef");
+    handleQuery();
+  }
+
+  /** 新增按钮操作 */
+  function handleAdd() {
+    reset();
+    open.value = true;
+    title.value = "添加收款单";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getReceiptVoucher(_id).then(response => {
+    form.value = response.data;
+  open.value = true;
+  title.value = "修改收款单";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["receiptVoucherRef"].validate(valid => {
+    if (valid) {
+      buttonLoading.value = true;
+      if (form.value.id != null) {
+        updateReceiptVoucher(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      } else {
+        addReceiptVoucher(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除收款单编号为"' + _ids + '"的数据项？').then(function() {
+    loading.value = true;
+    return delReceiptVoucher(_ids);
+  }).then(() => {
+    loading.value = true;
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {
+  }).finally(() => {
+    loading.value = false;
+  });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('financial/receiptVoucher/export', {
+    ...queryParams.value
+  }, `receiptVoucher_${new Date().getTime()}.xlsx`)
+}
+
+getList();
+</script>
